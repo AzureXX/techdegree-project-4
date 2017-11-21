e76f050559fe92c86309fd26bafa035e6c2af027
AzureXX <ismat.valiyev@gmail.com>
2017-11-21 23:49:53 +0400

    small fixes

--- js/main.js
+++ js/main.js
@@ -1,13 +1,13 @@
 $("#search").on("keyup change", function() {
-  var $input = $("#search").value.toUpperCase();
-  var  $col=$(".gallery-item");
+  var $input = $("#search").val.toUpperCase();
+  var $col=$(".gallery-item");
   var img;
   for(var i = 0; i < $col.length; i++ ) {
     var img = $col[i]
-    if(img.html().toUpperCase.indexOf($input) > -1) {
-      img.style.display = "";
+    if($(img).html().toUpperCase().indexOf($input) > -1) {
+      img.show;
     } else {
-      img.style.display = "none";
+      img.hide;
     }
   }
 });
