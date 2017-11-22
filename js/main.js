d7a92d151e8eb92331bdc0c4872fdeb5cda3b982
AzureXX <ismat.valiyev@gmail.com>
2017-11-22 14:20:31 +0400

    Finished project

--- js/main.js
+++ js/main.js
@@ -3,11 +3,17 @@ $("#search").on("keyup change", function() {
   var $col=$(".gallery-item");
   var img;
   for(var i = 0; i < $col.length; i++ ) {
-    var img = $col[i]
+    img = $col[i];
     if($(img).html().toUpperCase().indexOf($input) > -1) {
       img.style.display = "";
     } else {
-      img.style.display = "none";;
+      img.style.display = "none";
     }
   }
+});
+
+
+lightbox.option({
+  'wrapAround': true,
+  'alwaysShowNavOnTouchDevices':true,
 });
